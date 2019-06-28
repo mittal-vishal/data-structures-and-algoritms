011d56b1c5aa1694d1d31dab2242eab2bf3064eb
Vishal Mittal <vmittal@github.sie.sony.com>
2019-06-28 22:26:58 +0530

    Palindron or Not

--- src/com/vishal/recursion/PalindromeOrNot.java
+++ src/com/vishal/recursion/PalindromeOrNot.java
@@ -3,6 +3,9 @@ package com.vishal.recursion;
 public class PalindromeOrNot {
 	
 	public static boolean isPalin(String str, int start, int end) {
+		if(str == null || str == "") {
+			return false;
+		}
 		double mid = Math.ceil((start+end)/2);
 		if(str.charAt(start) == str.charAt(end) && end >= mid) {
 			if(end == mid) {
