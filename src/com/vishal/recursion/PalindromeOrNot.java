7b6a453c45166a3607047a3e7c0f14004024a83b
Vishal Mittal <vmittal@github.sie.sony.com>
2019-07-14 00:45:53 +0530

    -Modified with alternate logic

--- src/com/vishal/recursion/PalindromeOrNot.java
+++ src/com/vishal/recursion/PalindromeOrNot.java
@@ -1,16 +1,16 @@
 package com.vishal.recursion;
 
 public class PalindromeOrNot {
-	
+
 	public static boolean isPalin(String str, int start, int end) {
-		if(str == null || str == "") {
+		if (str == null || str == "") {
 			return false;
 		}
-		double mid = Math.ceil((start+end)/2);
-		if(str.charAt(start) == str.charAt(end) && end >= mid) {
-			if(end == mid) {
-				return true;
-			}
+		double mid = Math.ceil(str.length() / 2);
+		if (start >= mid || mid > end) {
+			return true;
+		}
+		if (str.charAt(start) == str.charAt(end)) {
 			return isPalin(str, ++start, --end);
 		}else {
 			return false;
