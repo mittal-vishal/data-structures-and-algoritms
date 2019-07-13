46919997b722c280e536be08ce3082bb19858943
Vishal Mittal <vmittal@github.sie.sony.com>
2019-07-14 00:07:55 +0530

    Permutation of given string

---
+++ src/com/vishal/recursion/Permutation.java
@@ -0,0 +1,34 @@
+package com.vishal.recursion;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Permutation {
+
+	private static List<String> returnList = new ArrayList<>();
+
+	public static List<String> permute(String str) {
+		return permute(str, 0, str.length() - 1);
+	}
+
+	public static List<String> permute(String str, int beg, int end) {
+		if (beg == end) {
+			returnList.add(str);
+		} else {
+			for (int i = beg; i <= end; i++) {
+				str = swapCharacter(str, beg, i);
+				permute(str, beg + 1, end);
+			}
+		}
+		return returnList;
+	}
+
+	private static String swapCharacter(String str, int i, int j) {
+		char charArray[] = str.toCharArray();
+		char temp = charArray[i];
+		charArray[i] = charArray[j];
+		charArray[j] = temp;
+		return String.valueOf(charArray);
+	}
+
+}
