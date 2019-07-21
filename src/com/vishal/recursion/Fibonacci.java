40bbbae13d354c4a8db321521870f0ac135f0708
Vishal Mittal <vmittal@github.sie.sony.com>
2019-07-21 22:17:28 +0530

    Fibonacci

--- src/com/vishal/recursion/Fibonacci.java
+++ src/com/vishal/recursion/Fibonacci.java
@@ -1,5 +1,36 @@
 package com.vishal.recursion;
 
 public class Fibonacci {
+	
+	private static int first;
+	private static int second;
+	
+	public static void printFibonacci(int n) {
+		printFibonacci(n, 0);
+	}
+	
+	private static void printFibonacci(int n, int i) {
+		if(n > i) {
+			if(i == 0) {
+				first = i;
+				System.out.println(first);
+				printFibonacci(n, ++i);
+			}else if(i == 1) {
+				second = i;
+				System.out.println(second);
+				printFibonacci(n, ++i);
+			}else {
+				int third = first + second;
+				System.out.println(third);
+				first = second;
+				second = third;
+				printFibonacci(n, ++i);
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		printFibonacci(10);
+	}
 
 }
