25ae09ee0c87c4c58a8c46748a60c3b6505c8571
Vishal Mittal <vmittal@github.sie.sony.com>
2019-07-21 22:18:37 +0530

    MaxLengthRope

---
+++ src/com/vishal/recursion/MaxLengthRope.java
@@ -0,0 +1,28 @@
+package com.vishal.recursion;
+
+public class MaxLengthRope {
+
+	public static int maxPieces(int n, int a, int b, int c) {
+		
+		if (n < 0) {
+			return -1;
+		} else if (n == 0) {
+			return 0;
+		}
+
+		else {
+			return 1 + maxOfThree(maxPieces(n - a, a, b, c), maxPieces(n - b, a, b, c), maxPieces(n - c, a, b, c));
+		}
+
+	}
+
+	private static int maxOfThree(int a, int b, int c) {
+		if (a >= b && a >= c) {
+			return a;
+		} else if (b >= a && b >= c) {
+			return b;
+		} else
+			return c;
+	}
+
+}
