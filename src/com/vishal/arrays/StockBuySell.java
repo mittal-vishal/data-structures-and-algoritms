1b28c3f01ce1f447fb8f0144c4aa4631f39a4168
Vishal Mittal <vmittal@github.sie.sony.com>
2019-08-10 12:36:56 +0530

    StockBuySell

---
+++ src/com/vishal/arrays/StockBuySell.java
@@ -0,0 +1,54 @@
+package com.vishal.arrays;
+
+public class StockBuySell {
+
+	public static void main(String[] args) {
+		int a[] = { 886, 2777, 6915, 7793, 8335, 5386, 492, 6649 };
+		stockBuySell(a, a.length);
+	}
+
+	private static void stockBuySell(int price[], int n) {
+		int minima[] = new int[price.length];
+		int maxima[] = new int[price.length];
+		Integer max = Integer.MIN_VALUE;
+		Integer min = Integer.MAX_VALUE;
+		for (int i = price.length - 1; i >= 0; i--) {
+			if (price[i] < max && price[i] < price[i + 1]) {
+				maxima[i] = max;
+			} else if (i == price.length - 1 || (price[i] > max && price[i] > price[i + 1])
+					|| (price[i] < max && price[i] > price[i + 1])) {
+				max = price[i];
+				maxima[i] = max;
+			} else {
+				maxima[i] = max;
+			}
+		}
+		for (int i = 0; i < price.length; i++) {
+			if (i == 0 || (price[i] < min && price[i] < price[i - 1])) {
+				min = price[i];
+				minima[i] = min;
+			} else if (price[i] > min && price[i] > price[i - 1]) {
+				minima[i] = min;
+			} else {
+				min = price[i];
+				minima[i] = min;
+			}
+		}
+		int count = 0;
+		for (int i = 0; i < price.length; i++) {
+			if ((price[i] == minima[i] || price[i] == maxima[i]) && (maxima[i] - minima[i] > 0)) {
+				count++;
+				if (count % 2 == 1) {
+					System.out.print("(" + i + " ");
+				} else {
+					System.out.print(i + ")" + " ");
+				}
+			}
+		}
+
+		if (count == 0) {
+			System.out.print("No Profit");
+		}
+	}
+
+}
