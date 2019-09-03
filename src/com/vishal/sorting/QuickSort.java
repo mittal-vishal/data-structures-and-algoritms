e0269ad73ed7edd8faa59963858f9c09a4560a03
Vishal Mittal <vmittal@github.sie.sony.com>
2019-09-03 11:27:53 +0530

    Quicksort - Sorting Technique

---
+++ src/com/vishal/sorting/QuickSort.java
@@ -0,0 +1,39 @@
+package com.vishal.sorting;
+
+public class QuickSort {
+
+	public static void main(String[] args) {
+		int a[] = { 12, 3, 5, 18, 7 };
+		sort(a, 0, a.length - 1);
+		for (int i : a) {
+			System.out.print(i + " ");
+		}
+	}
+
+	private static void sort(int a[], int beg, int end) {
+		if (beg < end) {
+			int p = partition(a, beg, end);
+			sort(a, beg, p - 1);
+			sort(a, p + 1, end);
+		}
+	}
+
+	private static int partition(int[] a, int beg, int end) {
+		int pivot = end;
+		int i = beg - 1;
+		int temp = 0;
+		for (int j = beg; j < end; j++) {
+			if (a[j] < a[pivot]) {
+				i++;
+				temp = a[i];
+				a[i] = a[j];
+				a[j] = temp;
+			}
+		}
+		temp = a[i + 1];
+		a[i + 1] = a[pivot];
+		a[pivot] = temp;
+		return i + 1;
+	}
+
+}
