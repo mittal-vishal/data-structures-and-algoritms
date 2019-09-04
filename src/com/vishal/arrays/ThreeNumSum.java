02ab8f969ef3a654ac2731618c69735ff1bf97aa
Vishal Mittal <vmittal@github.sie.sony.com>
2019-09-04 23:38:20 +0530

    Optimised

--- src/com/vishal/arrays/ThreeNumSum.java
+++ src/com/vishal/arrays/ThreeNumSum.java
@@ -5,32 +5,28 @@ import java.util.Arrays;
 public class ThreeNumSum {
 
 	public static void main(String[] args) {
-		int a[] = { 60, -65, 5, -21, 8, 93 };
+		int a[] = { 2, 3, 4, 40, 8, 20, 9 };
 		Arrays.sort(a);
-		System.out.println(findTriplet(a, 0));
+		System.out.println(findTriplet(a, 39));
 	}
 
-	private static String findTriplet(int[] a, int item) {
+	private static boolean findTriplet(int[] a, int item) {
 		Arrays.sort(a);
-		for (int i = 0; i < a.length-2; i++) {
-			if (find(a, i+1, a.length - 1, item - a[i]).equals("found")) {
-				return "found";
+		int left = -1, right = -1;
+		for (int i = 0; i < a.length - 2; i++) {
+			left = i + 1;
+			right = a.length - 1;
+			while (left < right) {
+				if (a[left] + a[right] + a[i] == item) {
+					return true;
+				} else if (a[left] + a[right] > item) {
+					right--;
+				} else {
+					left++;
+				}
 			}
 		}
-		return "not found";
-	}
-
-	private static String find(int[] a, int left, int right, int item) {
-		while (left != right) {
-			if (a[left] + a[right] == item) {
-				return "found";
-			} else if (a[left] + a[right] > item) {
-				right--;
-			} else {
-				left++;
-			}
-		}
-		return "not found";
+		return false;
 	}
 
 }
