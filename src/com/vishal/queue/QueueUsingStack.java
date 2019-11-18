d063e1f85d82ee868d08ab868e2263d82223aef4
Vishal Mittal <vmittal@github.sie.sony.com>
2019-11-18 12:35:50 +0530

    Queue related problems

---
+++ src/com/vishal/queue/QueueUsingStack.java
@@ -0,0 +1,31 @@
+/*package com.vishal.queue;
+
+import java.util.Arrays;
+
+public class QueueUsingStack {
+	
+	public static void main(String[] args) {
+		pop();
+		push(4);
+		push(1);
+		print();
+		pop();
+		push(15);
+		System.out.println();
+		print();
+	}
+	
+	private static void push(int item) {
+		
+	}
+	
+	private static int pop() {
+		
+	}
+	
+	private static void print() {
+		
+	}
+
+}
+*/
---
+++ src/com/vishal/queue/ReversingQueue.java
@@ -0,0 +1,10 @@
+package com.vishal.queue;
+
+public class ReversingQueue {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
