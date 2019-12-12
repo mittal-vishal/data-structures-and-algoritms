ef10babfd407b70eca11fbc92099397f3711f239
Vishal Mittal <vmittal@github.sie.sony.com>
2019-12-13 00:12:43 +0530

    BST related problems

--- src/com/vishal/binarysearchtree/BstOrNot.java
+++ src/com/vishal/binarysearchtree/BstOrNot.java
@@ -3,7 +3,7 @@ package com.vishal.binarysearchtree;
 import com.vishal.binarytree.Node;
 
 public class BstOrNot {
-	
+
 	private static Node rootNode = null;
 	private static Node prevNode = null;
 
@@ -16,22 +16,34 @@ public class BstOrNot {
 		rootNode.getRight().setRight(new Node(40));
 		rootNode.getRight().getRight().setLeft(new Node(30));
 		System.out.println(find(rootNode));
+		System.out.println(findUsingRange(rootNode, Integer.MIN_VALUE, Integer.MAX_VALUE));
 	}
 
 	private static boolean find(Node root) {
-		if(root != null){
-			if(!find(root.getLeft())) {
+		if (root != null) {
+			if (!find(root.getLeft())) {
 				return false;
 			}
-			if(prevNode != null && root.getData() < prevNode.getData()) {
+			if (prevNode != null && root.getData() < prevNode.getData()) {
 				return false;
 			}
 			prevNode = root;
-			if(!find(root.getRight())) {
+			if (!find(root.getRight())) {
 				return false;
 			}
 		}
 		return true;
 	}
-	
+
+	private static boolean findUsingRange(Node root, int min, int max) {
+		if (root == null) {
+			return true;
+		}
+		if (root.getData() < min || root.getData() > max) {
+			return false;
+		}
+		return (findUsingRange(root.getLeft(), min, root.getData())
+				&& findUsingRange(root.getRight(), root.getData(), max));
+	}
+
 }
