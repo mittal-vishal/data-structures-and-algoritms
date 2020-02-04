5d0f4436db7bf4c1851632a18d91f72fae62361c
Vishal Mittal <vmittal@github.sie.sony.com>
2020-02-04 15:44:28 +0530

    Heap related problems

---
+++ src/com/vishal/heap/HeapStruc.java
@@ -0,0 +1,30 @@
+package com.vishal.heap;
+
+public class HeapStruc {
+
+	char character;
+	int occurance;
+
+	public HeapStruc(char character, int occurance) {
+		super();
+		this.character = character;
+		this.occurance = occurance;
+	}
+
+	public char getCharacter() {
+		return character;
+	}
+
+	public void setCharacter(char character) {
+		this.character = character;
+	}
+
+	public int getOccurance() {
+		return occurance;
+	}
+
+	public void setOccurance(int occurance) {
+		this.occurance = occurance;
+	}
+
+}
