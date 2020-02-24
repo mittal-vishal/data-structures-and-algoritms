3bc93a007b4eb32d1460ee0eeb0d96827c09f115
Vishal Mittal <vmittal@github.sie.sony.com>
2020-02-24 22:56:57 +0530

    graph related problems

---
+++ src/com/vishal/graph/DFS.java
@@ -0,0 +1,47 @@
+package com.vishal.graph;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class DFS {
+
+	public static void main(String[] args) {
+		//Directed Graph, we considered disconnected cases
+		Scanner sc = new Scanner(System.in);
+		int nov = sc.nextInt();
+		int edges = sc.nextInt();
+		List<List<Integer>> adj = new ArrayList<>();
+		for (int i = 0; i < nov; i++) {
+			adj.add(new ArrayList<>());
+		}
+		int u = 0, v = 0;
+		for (int i = 0; i < edges; i++) {
+			u = sc.nextInt();
+			v = sc.nextInt();
+			adj.get(u).add(v);
+		}
+		sc.close();
+		boolean[] visited = new boolean[nov];
+		dfs(adj, visited, nov);
+	}
+
+	private static void dfs(List<List<Integer>> adj, boolean[] visited, int nov) {
+		for (int i = 0; i < nov; i++) {
+			if (visited[i] == false) {
+				dfsTraversal(adj, visited, i);
+			}
+		}
+	}
+
+	private static void dfsTraversal(List<List<Integer>> adj, boolean[] visited, int src) {
+		visited[src] = true;
+		System.out.print(src + " ");
+		for(int i : adj.get(src)) {
+			if(visited[i] == false) {
+				dfsTraversal(adj, visited, i);
+			}
+		}
+	}
+
+}
---
+++ src/com/vishal/graph/TopologicalSort.java
@@ -0,0 +1,52 @@
+package com.vishal.graph;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class TopologicalSort {
+
+	public static void main(String[] args) {
+		//Directed Acyclic Graph, we considered disconnected cases
+		Scanner sc = new Scanner(System.in);
+		int nov = sc.nextInt();
+		int edges = sc.nextInt();
+		List<List<Integer>> adj = new ArrayList<>();
+		for (int i = 0; i < nov; i++) {
+			adj.add(new ArrayList<>());
+		}
+		int u = 0, v = 0;
+		for (int i = 0; i < edges; i++) {
+			u = sc.nextInt();
+			v = sc.nextInt();
+			adj.get(u).add(v);
+		}
+		sc.close();
+		boolean[] visited = new boolean[nov];
+		topological(adj, visited, nov);
+	}
+
+	private static void topological(List<List<Integer>> adj, boolean[] visited, int nov) {
+		Stack<Integer> stack = new Stack<>();
+		for (int i = 0; i < nov; i++) {
+			if (visited[i] == false) {
+				dfsTraversal(adj, visited, i, stack);
+			}
+		}
+		while(!stack.isEmpty()) {
+			System.out.print(stack.pop() + " ");
+		}
+	}
+
+	private static void dfsTraversal(List<List<Integer>> adj, boolean[] visited, int src, Stack<Integer> stack) {
+		visited[src] = true;
+		for(int i : adj.get(src)) {
+			if(visited[i] == false) {
+				dfsTraversal(adj, visited, i, stack);
+			}
+		}
+		stack.push(src);
+	}
+
+}
