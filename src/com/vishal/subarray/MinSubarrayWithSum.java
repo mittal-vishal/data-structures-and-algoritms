873308ec918fb7f1f59e7af965dc82d15f6a7744
Vishal Mittal <vmittal@github.sie.sony.com>
2020-07-27 09:54:55 +0530

    Goldman Sachs Coderpad

--- src/com/vishal/subarray/MinSubarrayWithSum.java
+++ src/com/vishal/subarray/MinSubarrayWithSum.java
@@ -35,4 +35,33 @@ public class MinSubarrayWithSum {
 		}
 		return min_len;
 	}
+	
+	static int smallestSubWithSumGreater(int arr[], int n, int x)  
+    { 
+        // Initialize current sum and minimum length 
+        int curr_sum = 0, min_len = n + 1; 
+  
+        // Initialize starting and ending indexes 
+        int start = 0, end = 0; 
+        while (end < n)  
+        { 
+            // Keep adding array elements while current sum 
+            // is smaller than x 
+            while (curr_sum <= x && end < n) 
+                curr_sum += arr[end++]; 
+  
+            // If current sum becomes greater than x. 
+            while (curr_sum > x && start < n)  
+            { 
+                // Update minimum length if needed 
+                if (end - start < min_len) 
+                    min_len = end - start; 
+  
+                // remove starting elements 
+                curr_sum -= arr[start++]; 
+            } 
+        } 
+        return min_len; 
+    } 
+	
 }
