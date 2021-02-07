49b5e2f94f02799ffd328ce269ef413842906df6
vishalmittal <vishalmittal1993>
2021-02-07 18:10:53 +0530

    Palindrome in LinkedList

---
+++ src/com/vishal/linkedlist/Partition.java
@@ -0,0 +1,72 @@
+package com.vishal.linkedlist;
+
+public class Partition {
+
+    public SinglyNode partition(SinglyNode head, int k){
+        SinglyNode itr = head;
+        SinglyNode tail = head;
+        while(tail.next != null) {
+            tail = tail.next;
+        }
+        SinglyNode end = tail;
+        SinglyNode prev = null;
+        SinglyNode temp = null;
+        boolean  isFirst = true;
+        while(itr != end){
+            if(itr.data >= k) {
+                if (itr == head) {
+                    head = head.next;
+                } else {
+                    prev.next = itr.next;
+                }
+                temp = itr;
+                tail.next = temp;
+                itr = itr.next;
+                temp.next = null;
+                tail = temp;
+            }else{
+                prev = itr;
+                itr = itr.next;
+            }
+            if((itr == end) && isFirst){
+                end = itr.next;
+                isFirst = false;
+            }
+        }
+        return head;
+    }
+
+    public SinglyNode partition2(SinglyNode head, int x) {
+        SinglyNode lessS = null;
+        SinglyNode lessE = null;
+        SinglyNode greaterS = null;
+        SinglyNode greaterE = null;
+
+        SinglyNode curr = head;
+        while(curr != null){
+            if(curr.data < x){
+                if(lessS == null){
+                    lessS = curr;
+                    lessE = lessS;
+                }else{
+                    lessE.next = curr;
+                    lessE = lessE.next;
+                }
+            }else{
+                if(greaterS == null){
+                    greaterS = curr;
+                    greaterE = greaterS;
+                }else{
+                    greaterE.next = curr;
+                    greaterE = greaterE.next;
+                }
+            }
+        }
+        if(lessS == null || greaterS == null){
+            return head;
+        }
+        lessE.next = greaterS;
+        greaterE.next = null;
+        return lessS;
+    }
+}
