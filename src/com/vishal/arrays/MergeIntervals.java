1e9e2371099afe2eb4e31c54d0ef1ea08c29f5e6
vishalmittal <vishalmittal1993>
2021-04-04 14:04:33 +0530

    Merge Intervals Brute Force

---
+++ src/com/vishal/arrays/MergeIntervals.java
@@ -0,0 +1,52 @@
+package com.vishal.arrays;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MergeIntervals {
+
+    public int[][] merge(int[][] intervals) {
+
+        if(intervals == null || intervals.length < 2){
+            return intervals;
+        }
+
+        for(int i = 0; i < intervals.length; i++){
+            for(int j = 0; j < intervals.length; j++){
+                if(i != j){
+                    if(isOverLapped(intervals[i], intervals[j])){
+                        intervals[i][0] = Math.min(intervals[i][0], intervals[j][0]);
+                        intervals[j][0] = Math.min(intervals[i][0], intervals[j][0]);
+                        intervals[i][1] = Math.max(intervals[i][1], intervals[j][1]);
+                        intervals[j][1] = Math.max(intervals[i][1], intervals[j][1]);
+                    }
+                }
+            }
+        }
+
+        List<List<Integer>> uniqueIntervalsList = new ArrayList<>();
+        for(int[] interval: intervals){
+            if(!uniqueIntervalsList.contains(Arrays.asList(interval[0], interval[1]))){
+                uniqueIntervalsList.add(Arrays.asList(interval[0], interval[1]));
+            }
+        }
+
+        int[][] resultant = new int[uniqueIntervalsList.size()][2];
+        for(int i = 0; i < uniqueIntervalsList.size(); i++){
+            resultant[i][0] = uniqueIntervalsList.get(i).get(0);
+            resultant[i][1] = uniqueIntervalsList.get(i).get(1);
+        }
+
+        return resultant;
+    }
+
+    private boolean isOverLapped(int[] a, int[] b){
+        if(a[1] >= b[0] && a[0] <= b[1]){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+}
