3faa18f8ba057336a0b6fc236d3158575961e75d
vishalmittal <vishalmittal1993>
2021-05-13 10:07:33 +0530

    Two pointers approach

--- src/com/vishal/arrays/ThreeNumDistinctPair.java
+++ src/com/vishal/arrays/ThreeNumDistinctPair.java
@@ -1,51 +1,33 @@
 package com.vishal.arrays;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class ThreeNumDistinctPair {
 
 	public List<List<Integer>> threeSum(int[] nums) {
-		if (nums == null || nums.length == 0) {
-			return new ArrayList<>();
+		Set<List<Integer>> tripletSet = new HashSet<>();
+		if(nums == null || nums.length == 0) {
+			return new ArrayList<>(tripletSet);
 		}
-		List<List<Integer>> resList = new ArrayList<>();
-		List<Integer> innerList = null;
 		Arrays.sort(nums);
-		for (int i = 0; i < nums.length - 2; i++) {
-			if (i > 0 && nums[i] == nums[i - 1]) {
-				continue;
-			}
-			int left = i + 1;
-			int right = nums.length - 1;
-			int target = 0;
-			while (left < right) {
-				if ((left - i) > 1 && nums[left] == nums[left - 1]) {
+		int firstNum = 0, left = 0, right = 0;
+		for(int i = 0; i < nums.length - 2; i++) {
+			firstNum = nums[i];
+			left = i + 1;
+			right = nums.length - 1;
+			while(left < right){
+				if(firstNum + nums[left] + nums[right] == 0){
+					tripletSet.add(Arrays.asList(firstNum, nums[left], nums[right]));
+					left++; right--;
+				}else if(firstNum + nums[left] + nums[right] < 0){
 					left++;
-					continue;
-				}
-				if ((right < nums.length - 1) && nums[right] == nums[right + 1]) {
-					right--;
-					continue;
-				}
-				if (nums[i] + nums[left] + nums[right] == target) {
-					innerList = new ArrayList<>();
-					innerList.add(nums[i]);
-					innerList.add(nums[left]);
-					innerList.add(nums[right]);
-					left++;
-					right--;
-					resList.add(innerList);
-				} else if (nums[i] + nums[left] + nums[right] < target) {
-					left++;
-				} else {
+				}else{
 					right--;
 				}
 			}
 
 		}
-		return resList;
+		return new ArrayList(tripletSet);
 	}
 
 }
--- src/com/vishal/arrays/TwoNumberSum.java
+++ src/com/vishal/arrays/TwoNumberSum.java
@@ -1,25 +1,35 @@
 package com.vishal.arrays;
 
+import java.util.Arrays;
+
 public class TwoNumberSum {
 
 	public static void main(String[] args) {
 		int a[] = {2, 5, 8, 12, 30};
-		System.out.println(find(a, 42));
+		System.out.println(twoSum(a, 42));
 	}
 
-	private static String find(int[] a, int item) {
-		int left = 0; 
-		int right = a.length - 1;
-		while(left != right) {
-			if(a[left] + a[right] == item) {
-				return "found";
-			}else if(a[left] + a[right] > item) {
-				right--;
-			}else {
+	public static int[] twoSum(int[] numbers, int target) {
+		Arrays.sort(numbers);
+		int left = 0, right = numbers.length - 1;
+
+		int output[] = new int[2];
+		Arrays.fill(output, -1);
+
+		while(left < right){
+			if(numbers[left] + numbers[right] == target){
+				output[0] = left + 1;
+				output[1] = right + 1;
+				return output;
+			}else if(numbers[left] + numbers[right] < target){
 				left++;
+			}else{
+				right--;
 			}
 		}
-		return "not found";
+
+		return output;
+
 	}
 
 }
