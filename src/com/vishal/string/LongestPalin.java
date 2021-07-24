718e52ca88dc057e39901ee0d8a4dfd58b499458
vishalmittal <vishalmittal1993>
2021-07-24 21:52:03 +0530

    Palindrome and KMP

--- src/com/vishal/string/LongestPalin.java
+++ src/com/vishal/string/LongestPalin.java
@@ -1,43 +1,44 @@
 package com.vishal.string;
 
-import java.util.LinkedHashSet;
-import java.util.Scanner;
-import java.util.Set;
+
+import java.util.AbstractMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 
 public class LongestPalin {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int t = sc.nextInt();
-		while (t-- > 0) {
-			String str = sc.next();
-			System.out.print(findLongestPalin(str));
+
+	public String longestPalindrome(String s) {
+		if(s.length() < 2){
+			return s;
 		}
-		sc.close();
+		List<Map.Entry<Integer, Integer>> indicesEntryList = new LinkedList<>();
+		for(int i = 0; i < s.length() - 1; i++){
+			for(int j = i+1; j <= s.length(); j++){
+				indicesEntryList.add(new AbstractMap.SimpleEntry<>(i, j));
+			}
+		}
+		int max = 0;
+		String outputStr = "";
+		int resStart = -1, resEnd = -1;
+		String[] indicesArr = new String[2];
+		for(Map.Entry<Integer, Integer> entry: indicesEntryList){
+			int start = entry.getKey();
+			int end = entry.getValue();
+			if(isPalindrome(s, start, end - 1) && (end-start) > max){
+				resStart = start;
+				resEnd = end;
+				max = end-start;
+			}
+		}
+		return s.substring(resStart, resEnd);
 	}
 
-	private static String findLongestPalin(String str){
-	     Set<String> subStrSet = new LinkedHashSet<>();
-	     for(int i = 0; i < str.length(); i++){
-	         for(int j = i + 1; j <= str.length(); j++){
-	             subStrSet.add(str.substring(i,j));
-	         }
-	     }
-	     int max = Integer.MIN_VALUE;
-	     String returnStr = null;
-	     for(String st : subStrSet){
-	         if(isPalin(st, 0, st.length() - 1) && st.length() > max){
-	             max = st.length();
-	             returnStr = st;
-	         }
-	     }
-	     return returnStr;
-	 }
-
-	private static boolean isPalin(String s, int i, int j) {
+	private static boolean isPalindrome(String s, int i, int j) {
 		if (i > j) {
 			return true;
 		} else if (s.charAt(i) == s.charAt(j)) {
-			return isPalin(s, i + 1, j - 1);
+			return isPalindrome(s, i + 1, j - 1);
 		} else {
 			return false;
 		}
