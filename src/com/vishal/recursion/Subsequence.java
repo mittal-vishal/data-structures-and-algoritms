e2a3823c66745b4036b8e39d8fc5f738cc6cbeae
vishalmittal <vishalmittal1993>
2021-08-02 17:02:23 +0530

    Recursion Problems

--- src/com/vishal/recursion/Subsequence.java
+++ src/com/vishal/recursion/Subsequence.java
@@ -5,19 +5,27 @@ import java.util.List;
 
 public class Subsequence {
 
-    public static List<String> getSubset(String str){
-        List<String> subsetList = new ArrayList<>();
-        return getSubset(str, "", subsetList);
+    public static void main(String[] args) {
+        String str = "abc";
+        List<String> subList = getSubset(str);
+        System.out.print(subList);
     }
 
-    private static List<String> getSubset(String str, String current, List<String> subList) {
-        if(str.length() == 0) {
+    public static List<String> getSubset(String str){
+        List<String> subsetList = new ArrayList<>();
+        getSubset(str, "", subsetList, 0);
+        return subsetList;
+    }
+
+    private static void getSubset(String str, String current, List<String> subList, int i) {
+        if(i == str.length()) {
             subList.add(current);
-        }else {
-            getSubset(str.substring(1), current, subList);
-            getSubset(str.substring(1), current + str.charAt(0), subList);
+            return;
         }
-        return subList;
+        //Exclude
+        getSubset(str, current, subList, i+1);
+        //Include
+        getSubset(str, current.concat(String.valueOf(str.charAt(i))), subList, i+1);
     }
 
 }
