e7065a08f6f72e0b42084c685146e07ce51d9f3e
vishalmittal <vishalmittal1993>
2021-08-07 10:35:30 +0530

    Linked List problems

---
+++ src/com/vishal/linkedlist/RotateRight.java
@@ -0,0 +1,71 @@
+package com.vishal.linkedlist;
+
+public class RotateRight {
+
+    public SinglyNode rotateRightBruteForce(SinglyNode head, int k) {
+        if(head == null || head.next == null || k < 1){
+            return head;
+        }
+
+        SinglyNode curr = head;
+
+        int size = 0;
+
+        while(curr != null){
+            curr = curr.next;
+            size++;
+        }
+
+        k = k % size;
+        curr = head;
+
+        while(k-- > 0){
+            SinglyNode prev = null;
+
+            while(curr != null && curr.next != null){
+                prev = curr;
+                curr = curr.next;
+            }
+
+            prev.next = null;
+            curr.next = head;
+            head = curr;
+            curr = head;
+        }
+        return head;
+    }
+
+    public SinglyNode rotateRight(SinglyNode head, int k) {
+        if(head == null || head.next == null || k < 1){
+            return head;
+        }
+
+        SinglyNode curr = head;
+        SinglyNode tail = null;
+
+        int size = 0;
+
+        while(curr != null){
+            tail = curr;
+            curr = curr.next;
+            size++;
+        }
+
+        k = k % size;
+        curr = head;
+        int count = size - k - 1;
+
+        while(curr != null && count-- > 0 ){
+            curr = curr.next;
+        }
+
+        SinglyNode start = curr.next;
+        tail.next = head;
+        if(start != null){
+            head = start;
+        }
+        curr.next = null;
+        return head;
+    }
+
+}
