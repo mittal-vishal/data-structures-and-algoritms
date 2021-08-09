91d2306b1695bfe7f5a8bcc723b9bb768ae64da4
vishalmittal <vishalmittal1993>
2021-08-09 10:55:33 +0530

    LinkedList related problens

--- src/com/vishal/linkedlist/MergeSortedList.java
+++ src/com/vishal/linkedlist/MergeSortedList.java
@@ -3,46 +3,31 @@ package com.vishal.linkedlist;
 public class MergeSortedList {
 
     public SinglyNode mergeTwoLists(SinglyNode l1, SinglyNode l2) {
-        SinglyNode head = null;
-        SinglyNode start = null;
-        if(l1 == null && l2 == null){
-            return null;
-        }
-        while(l1 != null && l2 != null){
-            if(l1.data <= l2.data){
-                if(head == null){
-                    head = l1;
-                    start = l1;
-                    l1 = l1.next;
-                }else{
-                    head.next = l1;
-                    head = l1;
-                    l1 = l1.next;
-                }
-            }else{
-                if(head == null){
-                    head = l2;
-                    start = l2;
-                    l2 = l2.next;
-                }else{
-                    head.next = l2;
-                    head = l2;
-                    l2 = l2.next;
-                }
+        SinglyNode start = new SinglyNode(-1);
+        SinglyNode curr = start;
+        SinglyNode temp = null;
+        while(l1 != null || l2 != null){
+            if(l1 == null){
+                curr.next = l2;
+                break;
+            }else if(l2 == null){
+                curr.next = l1;
+                break;
+            } else if(l1.data <= l2.data){
+                temp = l1.next;
+                curr.next = l1;
+                curr = curr.next;
+                curr.next = null;
+                l1 = temp;
+            } else{
+                temp = l2.next;
+                curr.next = l2;
+                curr = curr.next;
+                curr.next = null;
+                l2 = temp;
             }
         }
-        if(l1 != null && head != null){
-            head.next = l1;
-        }else if(l1 != null && head == null){
-            head = l1;
-            start = head;
-        }else if(l2 != null && head != null){
-            head.next = l2;
-        }else{
-            head = l2;
-            start = head;
-        }
-        return start;
+        return start.next;
     }
 
 }
