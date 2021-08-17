faf54352f823b8f1f2458ed9019f7c6e80b01ef5
vishalmittal <vishalmittal1993>
2021-08-17 21:18:22 +0530

    Heap problem

--- src/com/vishal/heap/Insertion.java
+++ src/com/vishal/heap/Insertion.java
@@ -3,58 +3,61 @@ package com.vishal.heap;
 public class Insertion {
 
 	private static final int CAPACITY = 10;
-	private static int heap[] = null;
-	private static int size;
+	private int heap[];
+	private int size;
 
-	public static int getLeft(int i) {
+	public Insertion(){
+		heap = new int[CAPACITY];
+		size = 0;
+	}
+
+	public int getLeft(int i) {
 		return 2 * i + 1;
 	}
 
-	public static int getRight(int i) {
+	public int getRight(int i) {
 		return 2 * i + 2;
 	}
 
-	public static int getParent(int i) {
+	public int getParent(int i) {
 		return (i - 1) / 2;
 	}
 
-	public static void insert(int item) {
+	public void insert(int item) {
 		if (size == CAPACITY) {
 			System.out.println("Overflow");
-		} else {
-			heap[size++] = item;
-			int temp;
-			for (int i = size - 1; i > 0; i = getParent(i)) {
-				if (heap[i] < heap[getParent(i)]) {
-					temp = heap[i];
-					heap[i] = heap[getParent(i)];
-					heap[getParent(i)] = temp;
-				}
-			}
+			return;
 		}
+		heap[size] = item;
+		for(int i = size; i != 0 && heap[i] < heap[getParent(i)];){
+			int temp = heap[i];
+			heap[i] = heap[getParent(i)];
+			heap[getParent(i)] = temp;
+			i = getParent(i);
+		}
+		size++;
 	}
 
-	public static void display() {
+	public void display() {
 		for (int i = 0; i < size; i++) {
-			if (heap[i] != -1) {
+			if (heap[i] != 0) {
 				System.out.print(heap[i] + " ");
 			}
 		}
 	}
 
 	public static void main(String[] args) {
-		heap = new int[CAPACITY];
-		size = 0;
-		insert(10);
-		insert(20);
-		insert(15);
-		insert(40);
-		insert(50);
-		insert(100);
-		insert(25);
-		insert(45);
-		insert(12);
-		display();
+		Insertion heapInsertion = new Insertion();
+		heapInsertion.insert(10);
+		heapInsertion.insert(20);
+		heapInsertion.insert(15);
+		heapInsertion.insert(40);
+		heapInsertion.insert(50);
+		heapInsertion.insert(100);
+		heapInsertion.insert(25);
+		heapInsertion.insert(45);
+		heapInsertion.insert(1);
+		heapInsertion.display();
 	}
 
 }
