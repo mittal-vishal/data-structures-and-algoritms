3e76db74c2c99443c4d321a51a62b5bfd1d2d6a7
vishalmittal <vishalmittal1993>
2021-08-19 14:04:55 +0530

    Merge Interval Problem

--- src/com/vishal/mergeintervals/IntervalIntersection.java
+++ src/com/vishal/mergeintervals/IntervalIntersection.java
@@ -7,35 +7,34 @@ import java.util.List;
 public class IntervalIntersection {
 
     public static int[][] intervalIntersection(int[][] firstList, int[][] secondList) {
-        if(firstList == null || firstList.length == 0 || secondList == null || secondList.length == 0){
+        if(firstList == null || secondList == null ||
+                firstList.length == 0 || secondList.length == 0){
             return new int[0][0];
         }
-
-        List<List<Integer>> intersectionList = new ArrayList<>();
-        int i = 0, j = 0;
-
+        List<List<Integer>> intervalList = new ArrayList<>();
+        int i = 0;
+        int j = 0;
         while(i < firstList.length && j < secondList.length){
-            //Check for intersection
-            int max = Math.max(firstList[i][0], secondList[j][0]);
-            int min = Math.min(firstList[i][1], secondList[j][1]);
-            if(max <= min){
-                intersectionList.add(Arrays.asList(max, min));
+            int intersectStart = Math.max(firstList[i][0], secondList[j][0]);
+            int intersectEnd = Math.min(firstList[i][1], secondList[j][1]);
+            if(intersectStart <= intersectEnd){
+                intervalList.add(new ArrayList(Arrays.asList(intersectStart, intersectEnd)));
             }
-            if(firstList[i][1] < secondList[j][1]){
-                i++;
-            }else if(firstList[i][1] > secondList[j][1]){
+            if(firstList[i][1] > secondList[j][1]){
                 j++;
+            }else if(secondList[j][1] > firstList[i][1]){
+                i++;
             }else{
                 i++;
                 j++;
             }
         }
-        int[][] resultant = new int[intersectionList.size()][2];
-        for(i = 0; i < intersectionList.size(); i++){
-            resultant[i][0] = intersectionList.get(i).get(0);
-            resultant[i][1] = intersectionList.get(i).get(1);
+        int[][] res = new int[intervalList.size()][2];
+        for(int k = 0; k < intervalList.size(); k++){
+            res[k][0] = intervalList.get(k).get(0);
+            res[k][1] = intervalList.get(k).get(1);
         }
-        return resultant;
+        return res;
     }
 
     public static void main(String[] args) {
