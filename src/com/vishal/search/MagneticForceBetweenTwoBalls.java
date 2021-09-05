b799b6bfa4742b44da98e10e4ddff2a8aa694986
vishalmittal <vishalmittal1993>
2021-09-05 10:12:51 +0530

    Binary Search Related Problems

--- src/com/vishal/search/MagneticForceBetweenTwoBalls.java
+++ src/com/vishal/search/MagneticForceBetweenTwoBalls.java
@@ -7,10 +7,11 @@ public class MagneticForceBetweenTwoBalls {
     public int maxDistance(int[] position, int m) {
         int minF = 1;
         Arrays.sort(position);
-        int maxF = position[position.length - 1];
+        int maxF = position[position.length - 1] - position[0];
         int max = 1;
+
         while(minF <= maxF){
-            int mid = minF + (maxF - minF) / 2;
+            int mid = minF + (maxF-minF)/2;
             if(isPossible(position, m, mid)){
                 max = Math.max(max, mid);
                 minF = mid + 1;
@@ -18,22 +19,23 @@ public class MagneticForceBetweenTwoBalls {
                 maxF = mid - 1;
             }
         }
+
         return max;
     }
 
-    private boolean isPossible(int[] position, int m, int dist){
-        int prevStall = 0;
-        int stall = 1;
-        for(int i = 1; i < m; i++){
-            while(position[stall] - position[prevStall] < dist){
-                stall++;
-                if(stall == position.length){
-                    return false;
+    private boolean isPossible(int[] position, int m, int guess){
+        int currBallsCount = 1;
+        int prevPos = 0;
+        for(int i = 1; i < position.length; i++){
+            if(position[i] - position[prevPos] >= guess){
+                currBallsCount++;
+                if(currBallsCount == m){
+                    return true;
                 }
+                prevPos = i;
             }
-            prevStall = stall;
         }
-        return true;
+        return false;
     }
 
 }
