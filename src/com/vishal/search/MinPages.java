54081248657dd28f4bca8875ba7b399bc9c5a085
vishalmittal <vishalmittal1993>
2021-09-06 11:26:01 +0530

    Search Related Problems

--- src/com/vishal/search/MinPages.java
+++ src/com/vishal/search/MinPages.java
@@ -1,5 +1,18 @@
 package com.vishal.search;
 
+
+/*
+* Given number of pages in n different books and m students. The books are
+* arranged in ascending order of number of pages. Every student is assigned
+* to read some consecutive books. The task is to assign books in such a way
+* that the maximum number of pages assigned to a student is minimum
+*
+* Input : pages[] = {12, 34,67, 90} , m(number of students) = 2
+* Output = 113
+*
+* */
+
+
 public class MinPages {
 
     public static void main(String[] args) {
