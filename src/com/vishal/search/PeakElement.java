c8ab58d31bb4c5ea68d5f09b2fd7a620bf081a95
vishalmittal <vishalmittal1993>
2021-09-07 07:03:38 +0530

    Search Related Problems

--- src/com/vishal/search/PeakElement.java
+++ src/com/vishal/search/PeakElement.java
@@ -2,27 +2,27 @@ package com.vishal.search;
 
 public class PeakElement {
 
-	public static void main(String[] args) {
-		int a[] = { 10, 20, 3, 5, 23, 50, 67 };
-		System.out.println(find(a));
-	}
-
-	private static int find(int[] a) {
-		return find(a, 0, a.length - 1);
-	}
-
-	private static int find(int[] a, int beg, int end) {
-		int mid = (beg + end) / 2;
-		if ((0 == beg && a[mid] > a[mid + 1]) || (mid == a.length - 1 && a[mid] > a[mid - 1])
-				|| (end >= beg && a[mid] > a[mid - 1] && a[mid] > a[mid + 1])) {
-			return mid;
-		} else if (a[mid] < a[mid - 1]) {
-			return find(a, beg, mid - 1);
-		} else if(a[mid] < a[mid + 1]){
-			return find(a, mid + 1, end);
-		} else {
+	public int peakIndexInMountainArray(int[] arr) {
+		if(arr.length == 0){
 			return -1;
 		}
+		if(arr.length == 1){
+			return 0;
+		}
+		int start = 0;
+		int end = arr.length - 1;
+
+		while(start <= end){
+			int mid = start + (end - start)/2;
+			if(start == end){
+				return start;
+			}else if(arr[mid] < arr[mid + 1]){
+				start = mid + 1;
+			}else{
+				end = mid;
+			}
+		}
+		return start;
 	}
 
 }
