c4223cc3ed97e674c5ba3cb3e8271c88b3d476d7
vishalmittal <vishalmittal1993>
2021-09-07 22:30:13 +0530

    Sort Related Problems

--- src/com/vishal/sorting/Sort012.java
+++ src/com/vishal/sorting/Sort012.java
@@ -2,40 +2,26 @@ package com.vishal.sorting;
 
 public class Sort012 {
 
-	public static void main(String[] args) {
-		int a[] = {1,0,1,2,0,1,2,1};
-		a = sort(a);
-		for(int i : a) {
-			System.out.print(i + " ");
+	public void sortColors(int[] nums) {
+		int zeroIdx = 0;
+		int twoIdx = nums.length - 1;
+		for(int i = 0; i < nums.length; i++){
+			if(nums[i] == 0 && i > zeroIdx){
+				swap(nums, i, zeroIdx);
+				zeroIdx++;
+				i--;
+			}else if(nums[i] == 2 && i < twoIdx){
+				swap(nums, i, twoIdx);
+				twoIdx--;
+				i--;
+			}
 		}
 	}
 
-	private static int[] sort(int[] a) {
-		int temp = 0;
-		int beg = 0, i = 0, end = a.length-1;
-		while(i <= end) {
-			switch (a[i]) {
-			case 0:
-				temp = a[i];
-				a[i] = a[beg];
-				a[beg] = temp;
-				beg++;
-				i++;
-				break;
-			case 1:
-				i++;
-				break;
-			case 2:
-				temp = a[i];
-				a[i] = a[end];
-				a[end] = temp;
-				end--;
-				break;
-			default:
-				break;
-			}
-		}
-		return a;
+	private void swap(int[] nums, int i, int j){
+		int temp = nums[i];
+		nums[i] = nums[j];
+		nums[j] = temp;
 	}
 
 }
