efa9829a732e116c74e4e638fc0a9b5256b5419a
vishalmittal <vishalmittal1993>
2021-09-10 22:56:38 +0530

    Microsoft Problem

--- src/com/vishal/sorting/CountingSort.java
+++ src/com/vishal/sorting/CountingSort.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class CountingSort {
 
 	public static void main(String[] args) {
-		int a[] = { 1, 4, 1, 2, 7, 5, 2 };
+		int a[] = { 4, 5, 2, 3, 5, 2, 3, 8 };
 		a = sort(a);
 		for (int i : a) {
 			System.out.print(i + " ");
@@ -13,18 +13,28 @@ public class CountingSort {
 	}
 
 	private static int[] sort(int[] a) {
-		int range[] = new int[10];
+		int min = a[0];
+		int max = a[0];
+		for(int i = 0; i < a.length; i++){
+			min = Math.min(min, a[i]);
+			max = Math.max(max, a[i]);
+		}
+		int range = max - min + 1;
+		int occur[] = new int[range];
 		int output[] = new int[a.length];
-		Arrays.fill(range, 0);
-		for(int i = 0; i<a.length;i++) {
-			range[a[i]]++;
+
+		for(int i = 0; i < a.length; i++) {
+			int pos = a[i] - min;
+			occur[pos]++;
 		}
-		for(int i = 1; i<range.length;i++) {
-			range[i] = range[i] + range[i-1];
+		for(int i = 1; i < occur.length;i++) {
+			occur[i] = occur[i] + occur[i-1];
 		}
-		for(int i = 0; i<a.length;i++) {
-			output[range[a[i]]-1] = a[i];
-			range[a[i]]--;
+		for(int i = a.length - 1; i >= 0; i--) {
+			int pos = a[i] - min;
+			int correctIdx = occur[pos] - 1;
+			output[correctIdx] = a[i];
+			occur[pos]--;
 		}
 		return output;
 	}
