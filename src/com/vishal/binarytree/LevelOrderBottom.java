f135f8bcfe21bcdcebf243d4eead34e630b36091
vishalmittal <vishalmittal1993>
2021-09-28 06:53:12 +0530

    Tree Problems

---
+++ src/com/vishal/binarytree/LevelOrderBottom.java
@@ -0,0 +1,44 @@
+package com.vishal.binarytree;
+
+import java.util.*;
+
+public class LevelOrderBottom {
+
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> reverseLevelList = new ArrayList<>();
+        if(root == null){
+            return reverseLevelList;
+        }
+        List<Integer> levelList = new LinkedList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        Stack<List<Integer>> stack = new Stack<>();
+
+        queue.offer(root);
+        queue.offer(null);
+
+        while(!queue.isEmpty()){
+            TreeNode polled = queue.poll();
+            if(polled == null){
+                stack.push(levelList);
+                levelList = new LinkedList<>();
+                if(queue.size() > 0){
+                    queue.offer(null);
+                }
+            }else{
+                levelList.add(polled.data);
+                if(polled.left != null){
+                    queue.offer(polled.left);
+                }
+                if(polled.right != null){
+                    queue.offer(polled.right);
+                }
+            }
+        }
+        while(!stack.isEmpty()){
+            reverseLevelList.add(stack.pop());
+        }
+
+        return reverseLevelList;
+    }
+
+}
