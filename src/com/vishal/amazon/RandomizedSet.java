0e6c167e05ffd8f9b9569655db94575d284fe875
vishalmittal <vishalmittal1993>
2021-10-02 12:38:59 +0530

    Microsoft Problem

--- src/com/vishal/amazon/RandomizedSet.java
+++ src/com/vishal/amazon/RandomizedSet.java
@@ -1,50 +1,54 @@
 package com.vishal.amazon;
 
 import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
+import java.util.HashMap;
 import java.util.Random;
-import java.util.Set;
 
 class RandomizedSet {
 
     /** Initialize your data structure here. */
-    private Set<Integer> set;
-    private List<Integer> list;
-    
+    private HashMap<Integer, Integer> elementIndexMap;
+    private ArrayList<Integer> elementList;
+
     public RandomizedSet() {
-        set = new HashSet<>();
-        list = new ArrayList<>();
+        elementIndexMap = new HashMap<>();
+        elementList = new ArrayList<>();
     }
-    
+
     /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
     public boolean insert(int val) {
-        if(!set.contains(val)){
-            list.add(val);
-            set.add(val);
+        if(!elementIndexMap.containsKey(val)){
+            elementIndexMap.put(val, elementList.size());
+            elementList.add(elementList.size(), val);
             return true;
         }else{
             return false;
         }
     }
-    
+
     /** Removes a value from the set. Returns true if the set contained the specified element. */
     public boolean remove(int val) {
-        if(set.contains(val)){
-            set.remove(val);
+        if(elementIndexMap.containsKey(val)){
+            int index = elementIndexMap.get(val);
+            //Delete from map
+            elementIndexMap.remove(val);
+            //Delete from list
+            //Swap with last element
+            int lastElement = elementList.get(elementList.size()-1);
+            elementList.set(index, lastElement);
+            elementList.remove(elementList.size()-1);
+            if(elementIndexMap.containsKey(lastElement)){
+                elementIndexMap.put(lastElement, index);
+            }
             return true;
         }else{
             return false;
         }
     }
-    
+
     /** Get a random element from the set. */
     public int getRandom() {
-        if(!set.isEmpty()){
-            int rNum = new Random().nextInt(set.size());
-            return (int)set.stream().toArray()[rNum];
-        }else{
-            return -1;
-        }
+        Random random = new Random();
+        return elementList.get(random.nextInt(elementList.size()));
     }
 }
