14b2eb79b57d39635b4af3369e9dfad1cc757514
vishalmittal <vishalmittal1993>
2021-10-04 00:14:29 +0530

    Tree Problem

--- src/com/vishal/binarytree/InorderSuccessor.java
+++ src/com/vishal/binarytree/InorderSuccessor.java
@@ -2,39 +2,30 @@ package com.vishal.binarytree;
 
 public class InorderSuccessor {
 
-	private static TreeNode rootNode = null;
-
-	public static void main(String[] args) {
-		rootNode = new TreeNode(1);
-		rootNode.setLeft(new TreeNode(2));
-		rootNode.setRight(new TreeNode(3));
-		rootNode.getLeft().setLeft(new TreeNode(4));
-		rootNode.getLeft().setRight(new TreeNode(5));
-		rootNode.getRight().setLeft(new TreeNode(6));
-		rootNode.getRight().setRight(new TreeNode(7));
-		int num = 1;
-		find(rootNode, num);
-	}
-	
-	static class PreviousNode{
-		TreeNode prevNode;
-		public PreviousNode() {
-			prevNode = null;
+	private boolean isNext;
+	private TreeNode successor;
+	public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
+		if(root == null){
+			return null;
 		}
+		this.isNext = false;
+		inorderTraversal(root, p);
+		return successor;
 	}
 
-	private static void find(TreeNode root, int num) {
-		find(root, num, new PreviousNode());
-	}
-
-	private static void find(TreeNode root, int num, PreviousNode previousNode) {
-		if(root != null) {
-			find(root.getRight(), num, previousNode);
-			if(root.getVal() == num) {
-				System.out.println(previousNode.prevNode.getVal());
-			}
-			previousNode.prevNode = root;
-			find(root.getLeft(), num, previousNode);
+	private void inorderTraversal(TreeNode root, TreeNode p){
+		if(root.left != null){
+			inorderTraversal(root.left, p);
+		}
+		if(isNext){
+			successor = root;
+			isNext = false;
+		}
+		if(root.val == p.val){
+			isNext = true;
+		}
+		if(root.right != null){
+			inorderTraversal(root.right, p);
 		}
 	}
 
