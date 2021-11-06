92c5968500f6779bc6fd9ee683969195793279a9
vishalmittal <vishalmittal1993>
2021-11-06 13:30:05 +0530

    Tree Problems

--- src/com/vishal/binarysearchtree/CeilBST.java
+++ src/com/vishal/binarysearchtree/CeilBST.java
@@ -1,8 +1,10 @@
 package com.vishal.binarysearchtree;
 
 public class CeilBST {
-	
+
 	private static TreeNode rootNode = null;
+	private int ceil;
+	private boolean isFound;
 
 	public static void main(String[] args) {
 		rootNode = new TreeNode(5);
@@ -10,10 +12,30 @@ public class CeilBST {
 		rootNode.right  = new TreeNode(7);
 		rootNode.left.right = new TreeNode(2);
 		rootNode.left.right.right = new TreeNode(3);
-		System.out.println(find(rootNode, 3));
+		CeilBST ceil = new CeilBST();
+		int retVal = ceil.findCeil(rootNode, 3);
+		System.out.print(retVal);
 	}
 
-	private static int find(TreeNode root, int item) {
+	private int findCeil(TreeNode root, int item) {
+		ceil = -1;
+		dfs(root, item);
+		return ceil;
+	}
+
+	private void dfs(TreeNode root, int item){
+		if(root == null){
+			return;
+		}
+		dfs(root.left, item);
+		if(root.val >= item && (!isFound)){
+			ceil = root.val;
+			isFound = true;
+		}
+		dfs(root.right, item);
+	}
+
+	private int findCeilIteratively(TreeNode root, int item) {
 		int result = -1;
 		if(root.val > item) {
 			result = root.val;
--- src/com/vishal/binarysearchtree/ClosestElement.java
+++ src/com/vishal/binarysearchtree/ClosestElement.java
@@ -2,29 +2,25 @@ package com.vishal.binarysearchtree;
 
 public class ClosestElement {
 
-	private static TreeNode rootNode = null;
-	private static int result = Integer.MAX_VALUE;
+	private int closestEle;
+	private double closestVal;
 
-	public static void main(String[] args) {
-		rootNode = new TreeNode(82);
-		rootNode.left = new TreeNode(49);
-		rootNode.right = new TreeNode(94);
-		rootNode.left.left = new TreeNode(6);
-		rootNode.left.right = new TreeNode(51);
-		rootNode.left.right.right = new TreeNode(69);
-		rootNode.left.left.right = new TreeNode(13);
-		System.out.println(find(rootNode, 27));
+	public int closestValue(TreeNode root, double target) {
+		closestVal = Integer.MAX_VALUE;
+		inorder(root, target);
+		return closestEle;
 	}
 
-	private static int find(TreeNode root, int item) {
-		if(root != null) {
-			if(Math.abs(item - root.val) < result){
-				result = Math.abs(item - root.val);
-			}
-			find(root.left, item);
-			find(root.right, item);
+	private void inorder(TreeNode root, double target){
+		if(root == null){
+			return;
 		}
-		return result;
+		inorder(root.left, target);
+		if(Math.abs(target - root.val) < closestVal){
+			closestVal = Math.abs(target - root.val);
+			closestEle = root.val;
+		}
+		inorder(root.right, target);
 	}
 	
 }
