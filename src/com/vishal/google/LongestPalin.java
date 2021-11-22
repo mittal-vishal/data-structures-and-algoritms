b44db094b17af0c188b6ec254498f89efe98a0e1
vishalmittal <vishalmittal1993>
2021-11-22 08:28:41 +0530

    Google Problems

--- src/com/vishal/google/LongestPalin.java
+++ src/com/vishal/google/LongestPalin.java
@@ -3,31 +3,41 @@ package com.vishal.google;
 public class LongestPalin {
 
     public String longestPalindrome(String s) {
-        if(s.length() < 2){
-            return s;
+        if(s == null || s.length() == 0){
+            return "";
         }
-        String longest = "";
-        int maxLength = 0;
-        int start = -1, end = -1;
-        for(int i = 0; i < s.length(); i++){
-            int oddLen = expandAroundCenter(s, i, i);
-            int evenLen = expandAroundCenter(s, i, i+1);
-            int len = Math.max(oddLen, evenLen);
-            if(len > maxLength){
-                maxLength = len;
-                start = i - (len-1)/2;
-                end = i + len/2;
+        int maxLen = 0;
+        String longestPalin = "";
+        for(int i = 0; i < s.length();i++){
+            String currOddPalin = expandAroundIndex(s, i, true);
+            String currEvenPalin = expandAroundIndex(s, i, false);
+            if(currOddPalin.length() > maxLen){
+                maxLen = currOddPalin.length();
+                longestPalin = currOddPalin;
+            }
+            if(currEvenPalin.length() > maxLen){
+                maxLen = currEvenPalin.length();
+                longestPalin = currEvenPalin;
             }
         }
-        return s.substring(start, end + 1);
+        return longestPalin;
     }
 
-    private int expandAroundCenter(String s, int i, int j){
-        while(i >= 0 && j < s.length() && s.charAt(i) == s.charAt(j)){
-            i--;
-            j++;
+    private String expandAroundIndex(String s, int idx, boolean isOdd){
+        int i = idx;
+        int j = -1;
+        if(isOdd){
+            j = idx;
+        }else{
+            j = i + 1;
         }
-        return j - i -1;
+
+        while(i >= 0 && j < s.length() && s.charAt(i) == s.charAt(j)){
+            i--;j++;
+        }
+        i++;
+
+        return s.substring(i, j);
     }
 
 }
--- src/com/vishal/google/ShortestPalin.java
+++ src/com/vishal/google/ShortestPalin.java
@@ -3,32 +3,30 @@ package com.vishal.google;
 public class ShortestPalin {
 
     public static String shortestPalindrome(String s) {
-        if(s.length() < 2){
-            return s;
-        }
-        int suffixLen = generateLPS(s);
-        String revStr = new StringBuilder(s).reverse().substring(0, s.length() - suffixLen);
-        return new StringBuilder(revStr).append(s).toString();
+        int suffixLen = createPrefixSuffixTable(s); //abcd dcba
+        String strToAppend = new StringBuilder(s).reverse().substring(0, s.length() - suffixLen);
+        String ans = new StringBuilder(strToAppend).append(s).toString();
+        return ans;
     }
 
-    private static int generateLPS(String s){
+    private static int createPrefixSuffixTable(String s){
         StringBuilder revStr = new StringBuilder(s).reverse();
         s = new StringBuilder(s).append('#').append(revStr).toString();
         int[] lps = new int[s.length()];
-        int i = 0;
         lps[0] = 0;
+        int i = 0;
         int j = 1;
         while(j < s.length()){
             if(s.charAt(i) == s.charAt(j)){
                 lps[j] = i+1;
                 i++;
                 j++;
-            }else {
-                if (i == 0) {
+            }else{
+                if(i == 0){
                     lps[j] = 0;
                     j++;
-                } else {
-                    i = lps[i - 1];
+                }else{
+                    i = lps[i-1];
                 }
             }
         }
