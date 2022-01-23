65df9ce080f1dfb7b85d3dfa87de202f30f66246
vishalmittal <vishalmittal1993>
2022-01-23 13:07:51 +0530

    Microsoft

---
+++ src/com/vishal/microsoft/GreenDistrict.java
@@ -0,0 +1,73 @@
+package com.vishal.microsoft;
+
+import java.util.PriorityQueue;
+
+public class GreenDistrict {
+
+    private PriorityQueue<Integer> maxPq;
+    private PriorityQueue<Integer> minPq;
+    private int electricityPending;
+
+    public static void main(String[] args) {
+        int[] electricity = {2,2,1,2,2};
+        int X = 2;
+        int Y = 3;
+        GreenDistrict district = new GreenDistrict();
+        int minCost = district.getMinCost(electricity, X, Y);
+        System.out.println(minCost);
+    }
+
+    /*
+    * */
+    private int getMinCost(int[] electricity, int X, int Y){
+        int cost = 0;
+        maxPq = new PriorityQueue<>((a,b) -> b - a);
+        minPq = new PriorityQueue<>();
+        //Adding all units in priority queue
+        for(int unit: electricity){
+            maxPq.offer(unit);
+            minPq.offer(unit);
+            electricityPending += unit;
+        }
+        while(!maxPq.isEmpty()){
+            int currUnit = maxPq.peek();
+            if(currUnit <= 0){
+                break;
+            }
+            if(X >= Y || ((Y - X) < currUnit && electricityPending - (2*currUnit) >= 0 && maxPq.size() > 1)){
+                //Use type Y
+                cost += Y;
+                consumeElectricity(false);
+            }else{
+                //Use type X
+                cost += X;
+                consumeElectricity(true);
+            }
+        }
+        return cost;
+    }
+
+    private void consumeElectricity(boolean isTypeX){
+        if(isTypeX){
+            int removeUnit = maxPq.poll();
+            minPq.remove(removeUnit);
+            electricityPending -= removeUnit;
+        }else{
+            int extraUnits = maxPq.poll();
+            minPq.remove(extraUnits);
+            while(extraUnits > 0 && !minPq.isEmpty()){
+                int removeUnit = minPq.poll();
+                maxPq.remove(removeUnit);
+                if(extraUnits >= removeUnit){
+                    extraUnits -= removeUnit;
+                    electricityPending -= removeUnit;
+                }else{
+                    minPq.offer(removeUnit - extraUnits);
+                    maxPq.offer(removeUnit - extraUnits);
+                    break;
+                }
+            }
+        }
+    }
+
+}
