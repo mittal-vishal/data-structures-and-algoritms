ad490c980f1ca18c1a180737c397d37baf628705
vishalmittal <vishalmittal1993>
2023-04-18 19:34:00 +0530

    Preparation

--- src/com/vishal/search/SearchInRotatedArray.java
+++ src/com/vishal/search/SearchInRotatedArray.java
@@ -1,6 +1,33 @@
 package com.vishal.search;
 
 class SearchInRotatedArray {
+
+    public int searchOptimal(int[] nums, int target) {
+        int beg = 0;
+        int end = nums.length-1;
+        while(beg <= end){
+            int mid = beg + (end-beg)/2;
+            if(nums[mid] == target){
+                return mid;
+            }else if(nums[beg] <= nums[mid]){
+                //left part is sorted
+                if(target >= nums[beg] && target <= nums[mid]){
+                    end = mid-1;
+                }else{
+                    beg = mid+1;
+                }
+            }else{
+                //right part is sorted
+                if(target >= nums[mid] && target <= nums[end]){
+                    beg = mid+1;
+                }else{
+                    end = mid-1;
+                }
+            }
+        }
+        return -1;
+    }
+
     public int search(int[] nums, int target) {
         int min = findMin(nums);
         int searchElement = binarySearch(nums, 0, min-1, target);
